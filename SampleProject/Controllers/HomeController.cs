913e2524f1bff3b45e07601783821188858c1f2a
Muharrem Barkın <barkinmuharrem@outlook.com>
2022-03-01 13:27:41 +0300

    HomeController changed.

--- SampleProject/Controllers/HomeController.cs
+++ SampleProject/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using AmagiTech.GoogleReCaptcha.Attributes;
+﻿using AmagiTech.GoogleReCaptcha;
+using AmagiTech.GoogleReCaptcha.Attributes;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SampleProject.Models;
@@ -8,29 +9,46 @@ namespace SampleProject.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IGoogleReCaptchaManager _reCaptchaManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            IGoogleReCaptchaManager reCaptchaManager)
         {
             _logger = logger;
+            _reCaptchaManager = reCaptchaManager;
         }
 
-        
+
         public IActionResult Index()
         {
             return View();
         }
 
-        
+
         [HttpPost]
         [ServiceFilter(typeof(ReCaptchaAttribute))]
-        public IActionResult Index(SampleModel model)
+        public IActionResult WithAttribute(SampleModel model)
         {
             if (ModelState.IsValid)
             {
                 // Recaptcha İşlemini Geçti
                 return RedirectToAction("Success");
-            }    
-            return View(model);
+            }
+            return View("Index", model);
+        }
+
+        [HttpPost]
+        public IActionResult WithoutAttribute(SampleModel model)
+        {
+            var isValid = _reCaptchaManager.IsValid(HttpContext);
+            if (!isValid)
+                ModelState.AddModelError("Error", "Captcha validation failed!");
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Success");
+            }
+
+            return View("Index", model);
         }
 
         public string Success()
