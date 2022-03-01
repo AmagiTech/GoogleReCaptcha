538f0773dd7a6d6610272fb283be20b3775454e2
Muharrem Barkın <barkinmuharrem@outlook.com>
2022-03-01 13:25:52 +0300

    RecaptchaManager added

---
+++ AmagiTech.GoogleReCaptcha/GoogleReCaptchaManager.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace AmagiTech.GoogleReCaptcha
+{
+    public class GoogleReCaptchaManager : IGoogleReCaptchaManager
+    {
+        private readonly GoogleReCaptchaSettings reCaptchaSettings;
+        private readonly IGoogleReCaptchaGateway reCaptchaGateway;
+        public GoogleReCaptchaManager(GoogleReCaptchaSettings reCaptchaSettings,
+            IGoogleReCaptchaGateway reCaptchaGateway)
+        {
+            this.reCaptchaSettings = reCaptchaSettings;
+            this.reCaptchaGateway = reCaptchaGateway;
+        }
+
+        public bool IsValid(HttpContext context)
+        {
+            var isCaptchaValid = true;
+            if (reCaptchaSettings != null
+                && reCaptchaSettings.UseCaptcha)
+            {
+                StringValues _captchaResponse;
+                if (context.Request != null
+                    && context.Request.Form != null
+                    && context.Request.Form.TryGetValue("g-recaptcha-response", out _captchaResponse))
+                {
+                    var captchaResponse = _captchaResponse.ToString();
+                    isCaptchaValid = reCaptchaGateway.SiteVerify(captchaResponse);
+                }
+                else isCaptchaValid = false;
+            }
+            return isCaptchaValid;
+        }
+    }
+}
---
+++ AmagiTech.GoogleReCaptcha/IGoogleReCaptchaManager.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace AmagiTech.GoogleReCaptcha
+{
+    public interface IGoogleReCaptchaManager
+    {
+        bool IsValid(HttpContext context);
+    }
+}
